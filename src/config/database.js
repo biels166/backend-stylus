a22ec0fa424762eea199164351290bc09992d9b3
Gabriel Santana <gabriel_santana17@hotmail.com>
2024-11-19 18:09:33 +0000

    Atualização e coreção da conexão com mongo DB

--- src/config/database.js
+++ src/config/database.js
@@ -3,17 +3,16 @@ require('dotenv').config()
 
 let url = ""
 
-if (process.env.ENVCONFIG == "DEV") {
-    url = 'mongodb://localhost:27017/stylus'
-}
-else {
+if (process.env.ENVCONFIG === "PRD") {
     const user = process.env.MONGO_DB_USER
     const secret = process.env.MONGO_DB_SECRET
 
     url = `mongodb+srv://${user}:${secret}@clusterstylus.tfhgf.mongodb.net/?retryWrites=true&w=majority&appName=ClusterStylus`
 }
+else
+    url = 'mongodb://localhost:27017/stylus'
 
 
-mongoose.connect(url, { useNewUrlParser: true })
+mongoose.connect(url)
 
 module.exports = mongoose
